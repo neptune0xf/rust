8009a23f20eef689899e051c51d76816bf226edb
rosendo <rosendo2018@163.com>
2022-06-28 15:13:21 +0800

    fix:

--- .github/index.cjs
+++ .github/index.cjs
@@ -3,13 +3,17 @@ const path = require('path');
 main();
 async function main() {
   console.log(process.env);
-  const private1 = readFileSync(relativeToRootPath('./private1.pem'));
+  const private1 = readFileSync(
+    relativeToRootPath('./private1.pem')
+  ).toString();
   console.log('private1', private1);
-  const private2 = readFileSync(relativeToRootPath('./private2.pem'));
+  const private2 = readFileSync(
+    relativeToRootPath('./private2.pem')
+  ).toString();
   console.log('private2', private2);
   const admin = readFileSync(
     relativeToRootPath('.config/dfx/identity/admin/identity.pem')
-  );
+  ).toString();
   console.log('admin', admin);
 }
 
