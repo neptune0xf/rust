67c3a2fc282041e0d5d4a05acb701b70d1d1fb4e
rosendo <rosendo2018@163.com>
2022-06-28 15:54:31 +0800

    test

--- .github/index.cjs
+++ .github/index.cjs
@@ -3,18 +3,15 @@ const path = require('path');
 main();
 async function main() {
   console.log(process.env);
-  const private1 = readFileSync(
-    relativeToRootPath('./private1.pem')
-  ).toString();
-  console.log('private1', private1);
-  const private2 = readFileSync(
-    relativeToRootPath('./private2.pem')
-  ).toString();
-  console.log('private2', private2);
-  const admin = readFileSync(
+  const key = readFileSync(
     relativeToRootPath('.config/dfx/identity/admin/identity.pem')
   ).toString();
-  console.log('admin', admin);
+  key = key.split(' ').join('');
+  console.log('key', key);
+  writeFileSync(
+    relativeToRootPath('.config/dfx/identity/admin/identity.pem'),
+    key
+  );
 }
 
 function relativeToRootPath(url) {
