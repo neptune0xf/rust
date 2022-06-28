a4bb354640b41d098684ee14cc5b61181e2e53e7
rosendo <rosendo2018@163.com>
2022-06-28 14:55:17 +0800

    test

---
+++ .github/index.cjs
@@ -0,0 +1,13 @@
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+main();
+async function main() {
+  const private1 = readFileSync(relativeToRootPath('./private1.pem'));
+  console.log(private1);
+  const private2 = readFileSync(relativeToRootPath('./private2.pem'));
+  console.log(private2);
+}
+
+function relativeToRootPath(url) {
+  return path.resolve(process.HOME, url);
+}
